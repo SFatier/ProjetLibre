98c99c9c064dc7d19ebd039efe329b32566661f0
Ségolène FATIER <fatier-segolene@outlook.com>
2018-11-08 11:57:36 +0100

    Ajout de UserController et ses pages associés + procedure stockée

---
+++ Auth/Models/Users.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Models
+{
+    public class Users
+    {
+        public string UserId{ get; set; }
+        public string Username{ get; set; }
+        public string Email{ get; set; }
+        public string role{ get; set; }
+    }
+}
---
+++ Sql/DP_2k181108.sql
@@ -0,0 +1,57 @@
+GO
+CREATE PROCEDURE GetAllUsers
+	
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT *  from AspNetUsers
+END
+
+GO
+CREATE PROCEDURE GetUsersById
+	@paramId nvarchar(450)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT *  from AspNetUsers where Id = @paramId
+END
+
+GO
+CREATE PROCEDURE DeleteUser
+	@paramId nvarchar(450)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	delete AspNetUsers where Id = @paramId
+END
+
+GO
+CREATE PROCEDURE UpdateUser
+	@paramId nvarchar(450),
+	@paramUserName nvarchar(256),
+	@paramEmail nvarchar(256),
+	@paramPhone nvarchar(max)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    UPDATE [dbo].[AspNetUsers]
+	   SET [UserName] = @paramUserName		 
+		  ,[Email] = @paramEmail	 		  
+		  ,[PhoneNumber] = @paramPhone		
+	 WHERE Id = @paramId
+END
